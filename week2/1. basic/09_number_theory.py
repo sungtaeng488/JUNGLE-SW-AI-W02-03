8606730286236f47a3f71990433597d4bd15e10f
sungtaeng488 <sungtaeng4884835@gmail.com>
2026-08-29 16:20:15 +0900

    solve: 이분 탐색 문제 해결

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -23,7 +23,7 @@
 - 유클리드 호제법: gcd(a, b) = gcd(b, a % b)
 - LCM 공식: lcm(a, b) = (a × b) / gcd(a, b)
 """
-
+import math
 def gcd(a, b):
     """
     유클리드 호제법을 사용한 최대공약수 계산
@@ -34,10 +34,10 @@ def gcd(a, b):
     Returns:
         최대공약수
     """
-    # TODO: 유클리드 호제법 구현
-    # base case: b가 0이면 a 반환
-    # recursive를 이용 
-    pass
+    if b == 0:
+        return a
+    return gcd(b,a%b)
+    
 
 def gcd_iterative(a, b):
     """
@@ -49,9 +49,9 @@ def gcd_iterative(a, b):
     Returns:
         최대공약수
     """
-    # TODO: 반복문으로 구현
-    # b가 0이 될 때까지 반복
-    pass
+    while b != 0:
+        a,b = b , a%b
+    return gcd(a,b)
 
 def lcm(a, b):
     """
@@ -59,12 +59,15 @@ def lcm(a, b):
     
     Args:
         a, b: 두 양의 정수
+        - LCM 공식: lcm(a, b) = (a × b) / gcd(a, b)
     
     Returns:
         최소공배수
     """
-    # TODO: LCM 계산
-    pass
+    
+    return (a*b)//gcd(a,b)
+  
+    
 
 def extended_gcd(a, b):
     """
@@ -77,11 +80,15 @@ def extended_gcd(a, b):
     Returns:
         (gcd, x, y) 튜플
     """
-    # TODO: 확장 유클리드 호제법 구현
-    # base case: b가 0이면 (a, 1, 0) 반환    
-    # recursive case
-    # 역추적하며 x, y 계산
-    pass
+    if b == 0:
+        return (a,1,0)
+    g,x1,y1 = extended_gcd(b,a%b)
+    x = y1
+    y = x1 - (a//b)*y1
+
+    return (g,x,y)
+
+   
 
 def is_prime(n):
     """
@@ -92,12 +99,24 @@ def is_prime(n):
     
     Returns:
         소수이면 True, 아니면 False
-    """
-    # TODO: 소수 판별 구현
-    # n이 2보다 작으면 False
-    # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
-    # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+   """
+    if n<2 :
+        return False
+      
+    if n == 2:
+        return True
+
+    
+    if n % 2 == 0:
+        return False
+
+    
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+
+    return True
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
