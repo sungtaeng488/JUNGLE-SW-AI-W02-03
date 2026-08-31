a28772fb0245a4e3850c0fe799a35339c94e77ba
sungtaeng488 <sungtaeng4884835@gmail.com>
2026-08-31 17:19:11 +0900

    solve:advanced 3 번 문제 해결

--- week2/2. advanced/03_priority_queue.py
+++ week2/2. advanced/03_priority_queue.py
@@ -36,21 +36,25 @@ def process_emergency_room(patients):
     Returns:
         처리된 환자 순서
     """
-    # TODO: 빈 힙 생성
+   
     heap = []
-    
-    
-    # TODO: 모든 환자를 힙에 추가
-    pass
-        
+
+    for i in range(len(patients)):
+        heapq.heappush(heap, (patients[i][1], patients[i][0]))
+
     processed = []
-    
-    # TODO: 힙이 비어있지 않은 동안 반복
+
+    while len(heap) != 0:
+        priority, name = heapq.heappop(heap)
+        print(f"처리: {name} (우선순위: {priority})")
+        processed.append(name)
+
+    return processed
+
+        
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
     ## 환자 처리
-    pass
-        
-    return processed
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
